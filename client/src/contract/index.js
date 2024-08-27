058318867a8274d0daf0f51d3f792824f21d76db
EkaterinaGorbunova <ekaterina.nalitova@gmail.com>
2024-08-27 02:23:58 -0700

    removed non-existent icon

--- client/src/contract/index.js
+++ client/src/contract/index.js
@@ -1,3 +1,3 @@
-import contract from './AVAXGods.json'
-export const ADDRESS = '0x665b3f627F1720736b45d2E98aCda7dF84C2FF3C'
+import contract from "./AVAXGods.json"
+export const ADDRESS = "0x665b3f627F1720736b45d2E98aCda7dF84C2FF3C"
 export const { abi: ABI } = contract;
